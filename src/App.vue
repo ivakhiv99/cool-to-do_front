888ebe61fb3a6b738b4d4c84f50d610108397447
Vitaliy <ivahiv99@gmail.com>
2024-03-25 11:58:40 +0200

    config: configured eslint and prettier

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,31 @@
 <template>
-  <TicketList />
+	<TicketList />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import TicketList from "./components/TicketList.vue";
+import { defineComponent } from 'vue'
+import TicketList from './components/TicketList.vue'
 
 export default defineComponent({
-  name: "App",
-  components: {
-    TicketList,
-  },
-});
+	name: 'App',
+	components: {
+		TicketList,
+	},
+})
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 body {
-  margin: 0;
+	margin: 0;
 }
 * {
-  box-sizing: border-box;
+	box-sizing: border-box;
 }
 </style>
--- src/components/Ticket.vue
+++ src/components/Ticket.vue
@@ -1,36 +1,36 @@
 <template>
-    <div class="ticket_item_wrapper">
-        <input type="checkbox" :checked="ticketData.isDone" />
-        <p>{{ ticketData.text }}</p>
-    </div>
+	<div class="ticket_item_wrapper">
+		<input type="checkbox" :checked="ticketData.isDone" />
+		<p>{{ ticketData.text }}</p>
+	</div>
 </template>
 
 <script lang="ts">
 export default {
-    props: ['ticketData'],
+	props: ['ticketData'],
 }
 </script>
 
 <style lang="scss">
 .ticket_item_wrapper {
-    width: 450px;
-    height: 150px;
-    padding: 15px 20px 15px 35px;
-    background-color: #fff;
-    border-radius: 20px;
-    margin-bottom: 20px;
+	width: 450px;
+	height: 150px;
+	padding: 15px 20px 15px 35px;
+	background-color: #fff;
+	border-radius: 20px;
+	margin-bottom: 20px;
 
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
 
-    p {
-        font-size: 22px;
-        font-weight: 700;
-    }
+	p {
+		font-size: 22px;
+		font-weight: 700;
+	}
 
-    input[type='checkbox'] {
-        margin-right: 15px;
-    }
+	input[type='checkbox'] {
+		margin-right: 15px;
+	}
 }
 </style>
